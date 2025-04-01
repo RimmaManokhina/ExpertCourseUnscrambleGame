b2797c8aeab128ddc6b61dcea90e5dd89f95b2d7
Rimma_M <rimmafon@gmail.com>
2025-04-01 11:38:14 +0300

    UG-22-CustomView + XML

---
+++ app/src/main/java/com/github/cawboyroy/expertcoursestudy/views/retry/RetryButton.kt
@@ -0,0 +1,44 @@
+package com.github.cawboyroy.expertcoursestudy.views.retry
+
+import android.content.Context
+import android.os.Parcelable
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatButton
+import com.github.cawboyroy.expertcoursestudy.views.visiblebutton.UpdateVisibility
+import com.github.cawboyroy.expertcoursestudy.views.visiblebutton.VisibilitySavedState
+import com.github.cawboyroy.expertcoursestudy.views.visiblebutton.VisibilityUiState
+
+class RetryButton  : AppCompatButton, UpdateVisibility {
+
+    private lateinit var state: VisibilityUiState
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+        override fun onSaveInstanceState(): Parcelable? {
+            return super.onSaveInstanceState()?.let {
+                val savedState = VisibilitySavedState (it)
+                savedState.save(state)
+                return savedState
+            }
+        }
+
+        override fun onRestoreInstanceState(state: Parcelable?) {
+            val restoredState = state as VisibilitySavedState
+            super.onRestoreInstanceState(restoredState.superState)
+            update(restoredState.restore())
+        }
+
+    override fun update(state: VisibilityUiState) {
+        this.state = state
+        this.state.update(this)
+    }
+
+    override fun update(visibility: Int) {
+        this.visibility = visibility
+    }
+}
